82492262f380483739747311ec4c923d198a0dff
Tim Guo <gtm1224@berkeley.edu>
2021-11-15 22:59:01 -0800

    generate users and add backend user center

--- app.py
+++ app.py
@@ -77,3 +77,34 @@ def register():
 @app.context_processor
 def account():
     return {"username":username}
+
+
+def createBatchUsers():
+    words = list("abcdefghijklmnopqrstuvwxyz")
+    citys = ["010","021","0512"]
+    mylikes = ["睡觉","旅游","看书","唱歌"]
+    import random
+    for i in range(100):
+        random.shuffle(words)
+        username="".join(words[:6])
+        sex = random.randint(0,1)
+        city = citys[random.randint(0,2)]
+        random.shuffle(mylikes)
+        mylike = "|".join(mylikes[:random.randint(0,3)])
+        user = User(
+            realname="-",
+            username=username,
+            password="",
+            sex=sex,
+            mylike=mylike,
+            city=city,
+            intro=""
+        )
+        db.session.add(user)
+    db.session.commit()
+
+# get user list
+@app.route("/userlist",methods=['get'])
+def userList():
+    users = User.query.all()
+    return render_template("user/user_list.html",users=users)
