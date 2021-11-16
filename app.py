809e47bb38a3f8e1b863c82fb1559c9a029164df
Tim Guo <gtm1224@berkeley.edu>
2021-11-16 08:41:04 -0800

    edit and delete, filter functions added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -104,7 +104,47 @@ def createBatchUsers():
     db.session.commit()
 
 # get user list
-@app.route("/userlist",methods=['get'])
+@app.route("/userlist",methods=['get','post'])
 def userList():
-    users = User.query.all()
+    if request.method == "POST":
+        q = request.form['q']
+        # print(q)
+        # condition = {request.form['field']:q}
+        # print(condition)
+        if request.form['field'] == "realname":
+            condition = User.realname.like('%%%s%%' % q)
+            print("<<<<<<>>>>>>>>")
+            print(('%%%s%%' % q))
+        else:
+            condition = User.username.like('%%%s%%' % q)
+    # use filter_by
+    #     users = User.query.filter_by(**condition).all()
+    # use filter like
+        users = User.query.filter(condition).all()
+
+    else:
+        users = User.query.all()
     return render_template("user/user_list.html",users=users)
+
+# delete user by id:
+@app.route("/user_delete/<int:user_id>")
+def deleteUser(user_id):
+    user = User.query.get(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for("userList"))
+
+# edit user info:
+@app.route("/useredit/<int:user_id>",methods=['get','post'])
+def editUser(user_id):
+    user = User.query.get(user_id)
+    if request.method == "POST":
+        user.username = request.form['username']
+        user.realname = request.form['name']
+        user.sex = request.form['sex']
+        user.mylike = "|".join(request.form.getlist('like'))
+        user.city = request.form['city']
+        user.intro = request.form['intro']
+        db.session.commit()
+        return redirect(url_for("userList"))
+    return render_template("user/edit_user.html",user=user)
