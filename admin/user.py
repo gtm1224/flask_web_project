9aa548c7344c16a37af8f68d363483f9ff755b73
Tim Guo <gtm1224@berkeley.edu>
2021-12-09 09:28:20 -0800

    modified user list

--- admin/user.py
+++ admin/user.py
@@ -15,71 +15,45 @@ def validate_username(username):
 # 如果用户刚进入列表页是访问http://127.0.0.1/user/list
 # 与"/list/<int:page>"不匹配，提供一个默认带有page默认值
 # 的路由
-@admin_app.route("/user/list/<int:page>",methods=['get',"post"])
-@admin_app.route("/user/list",defaults={"page":1},methods=['get',"post"])
+@admin_app.route("/user/list/<int:page>", methods=['get', "post"])
+@admin_app.route("/user/list", defaults={"page":1},methods=['get', "post"])
 def userList(page):
     if request.method == "POST":
         q = request.form['q']
-        # print(q)
-        # condition = {request.form['field']:q}
-        # print(condition)
-        field = request.form['field']
-        if field == "realname":
+        condition = {request.form['field']:q}
+        #filter_by
+        # users = User.query.filter_by(**condition).all()
+        # filter
+        # like
+        if request.form['field'] == "realname":
             condition = User.realname.like('%%%s%%' % q)
-            # print("<<<<<<>>>>>>>>")
-            # print(('%%%s%%' % q))
         else:
-            # use filter like
             condition = User.username.like('%%%s%%' % q)
-            # use filter_by
-            #users = User.query.filter_by(**condition).all()
-        order_type = request.form['order']
-        if order_type == "1":
+        if request.form['order'] == "1":
             order = User.id.asc()
         else:
             order = User.id.desc()
-        users = User.query.filter(condition,User.sex==request.form['sex']).order_by(order).paginate(page,5)
-        query_string =""
+
+        res = User.query.filter(condition,
+                                User.sex==request.form['sex'])\
+                                .order_by(order)\
+                                .paginate(page, 10)
+
+
     else:
-        # return redirect(url_for(userList2))
         # users = User.query.all()
-        # add pages
-        page = request.args.get('page',1)
-        # print('wwwwwww',page)
-        # 如果提交了查询关键词q
-        q = request.args.get('q')
-        print("qqqqqqqqqqqqqqqqqqqqqqq",q)
-        if q:
-            print("ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss")
-            field = request.args.get('field')
-            print(field)
-            if field == "realname":
-                condition = User.realname.like('%%%s%%' % q)
-            else:
-                condition = User.username.like('%%%s%%' % q)
 
-            order_type = request.args.get('order')
-            if order_type == "1":
-                order = User.id.asc()
-            else:
-                order = User.id.desc()
+        res = User.query.paginate(page,10)
+    # 无论搜索还是默认查看，都是翻页处理
+    users = res.items
+    pageList = res.iter_pages()
 
-            sex = request.args.get('sex', 1)
-            users = User.query.filter(condition, User.sex == sex).order_by(order).paginate()
-            # 拼接查询条件（相当于request的query_string属性）
-            # 在模版的翻页部分添加上query_string
-            query_string = "q=" + q + "&field=" + field + "&sex=" + sex + "&order=" + order_type + "&page="+ page
-            print("sssssssss",query_string)
-        else:
-            users = User.query.paginate(int(page), 10)
-            query_string = ""
-        print(query_string)
-    return render_template("admin/user/user_list.html", users=users.items,
-                               pages=users.pages,
-                               total=users.total,
-                               pageList=users.iter_pages(),
-                               condition=query_string
-                               )
+
+    return render_template("admin/user/user_list.html", users=users,
+                           pageList=pageList, pages=res.pages,
+                           total=res.total
+                           )
+
 
 # 根据用户id删除用户
 @admin_app.route("/delete/<int:user_id>")
